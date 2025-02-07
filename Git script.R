1bece628a8341cbd40409ce75143e0882827f1ab
EricStokan <ericjstokan@gmail.com>
2025-02-06 20:24:19 -0500

    Initial commit.

---
+++ Git script.R
@@ -0,0 +1,11 @@
+
+#You must firs install github
+# This varies depending if you are on a Windows or a Mac
+
+#Library for github
+library(usethis)
+
+#This email has to match
+use_git_config(user.name = "EricStokan", user.email = "ericjstokan@gmail.com")
+
+#Create a new repp and then click code
